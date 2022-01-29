89bade604745a018bded745ead0fee75a86c5ef5
Ulil Albab Surya Negara <surya.negara@gontor.ac.id>
2022-01-29 19:34:20 +0700

    heading block issue

--- components/index/heading.tsx
+++ components/index/heading.tsx
@@ -5,7 +5,7 @@ export default function HomeHeading(){
         <div className="whitespace-pre-line sm:mx-0 font-bold text-6xl md:text-6xl xl:text-9xl mt-24 xl:mt-48 mb-10 xl:mb-16 text-gray-800" style={{ lineHeight: '1.3'}}>
             Hello! I&apos;m <RoughNotation type="underline" show strokeWidth={2} color="#999999" padding={5}>Koalixious,</RoughNotation><br />
             <span className="heading">
-                a Frontend Engineer<br/>
+                a Frontend Engineer
             </span>
             &#38; Designer. 👋
         </div>
